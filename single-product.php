9a9da268d09fbb479cef35c2ea64bee3ebf68483
Wasileuss <wasyl.lviv@gmail.com>
2025-08-25 10:22:58 +0300

    update single-product

--- single-product.php
+++ single-product.php
@@ -13,51 +13,49 @@ $full_description = get_field('full_description', get_the_ID());
 
 <!-- MAIN CONTENT -->
 <main id="wcl-page-content" class="wcl-page-content">
-    <div class="wcl-single wcl-container">
-        <div class="wcl-single__container wcl-section-inner">
-            <div class="wcl-state__product wcl-state__product--single">
-                <div class="wcl-state__product-content">
-                    <h2 class="wcl-state__product-title wcl-state__product-title--single"><?php the_title(); ?></h2>
-                    <div class="wcl-state__product-info">
-                        <p class="wcl-state__product-info-top">
-                            <?php if ($btu): ?>
-                                <span><?php echo esc_html($btu); ?> BTU |</span>
-                            <?php endif; ?>
-                            <?php if ($pump): ?>
-                                <span><?php echo esc_html($pump); ?></span>
-                            <?php endif; ?>
-                        </p>
-                        <p class="wcl-state__product-info-middle">
-                            <?php if ($volt): ?>
-                                <span><?php echo esc_html($volt); ?> Volt |</span>
-                            <?php endif; ?>
-                            <?php if ($amper): ?>
-                                <span><?php echo esc_html($amper); ?> Amp |</span>
-                            <?php endif; ?>
-                            <?php if ($controls): ?>
-                                <span><?php echo esc_html($controls); ?></span>
-                            <?php endif; ?>
-                        </p>
-                        <?php if ($additional_info): ?>
-                            <p class="wcl-state__product-info-bottom"><?php echo esc_html($additional_info); ?></p>
+    <div class="wcl-single__container wcl-section-inner">
+        <div class="wcl-single__product">
+            <div class="wcl-single__product-content">
+                <h2 class="wcl-single__product-title"><?php the_title(); ?></h2>
+                <div class="wcl-single__product-info">
+                    <p>
+                        <?php if ($btu): ?>
+                            <span><?php echo esc_html($btu); ?> BTU |</span>
                         <?php endif; ?>
-                    </div>
+                        <?php if ($pump): ?>
+                            <span><?php echo esc_html($pump); ?></span>
+                        <?php endif; ?>
+                    </p>
+                    <p>
+                        <?php if ($volt): ?>
+                            <span><?php echo esc_html($volt); ?> Volt |</span>
+                        <?php endif; ?>
+                        <?php if ($amper): ?>
+                            <span><?php echo esc_html($amper); ?> Amp |</span>
+                        <?php endif; ?>
+                        <?php if ($controls): ?>
+                            <span><?php echo esc_html($controls); ?></span>
+                        <?php endif; ?>
+                    </p>
+                    <?php if ($additional_info): ?>
+                        <p><?php echo esc_html($additional_info); ?></p>
+                    <?php endif; ?>
                 </div>
-                <?php if (!empty($full_description)): ?>
-                    <div class="wcl-state__product-full-info wcl-state__product-full-info--single">
-                        <?= wp_kses_post($full_description); ?>
-                    </div>
-                <?php endif; ?>
-                <?php if ($images): ?>
-                    <div class="wcl-state__product-gallery wcl-state__product-gallery--single">
-                        <?php foreach ($images as $image): ?>
-                            <div class="wcl-state__product-image">
-                                <?= wp_get_attachment_image($image, 'landscape-sm'); ?>
-                            </div>
-                        <?php endforeach; ?>
-                    </div>
-                <?php endif; ?>
             </div>
+            <?php if (!empty($full_description)): ?>
+                <div class="wcl-single__product-full-info">
+                    <?= wp_kses_post($full_description); ?>
+                </div>
+            <?php endif; ?>
+            <?php if ($images): ?>
+                <div class="wcl-single__product-gallery">
+                    <?php foreach ($images as $image): ?>
+                        <div class="wcl-single__product-image">
+                            <?= wp_get_attachment_image($image, 'landscape-sm'); ?>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            <?php endif; ?>
         </div>
     </div>
 </main>
